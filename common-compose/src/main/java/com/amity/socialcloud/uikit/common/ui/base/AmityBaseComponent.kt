322506245f961220ab1fe97e56b9964663f9acfa
enixma <Enixma@users.noreply.github.com>
2025-09-03 15:38:17 +0700

    chore: apply new version number

--- common-compose/src/main/java/com/amity/socialcloud/uikit/common/ui/base/AmityBaseComponent.kt
+++ common-compose/src/main/java/com/amity/socialcloud/uikit/common/ui/base/AmityBaseComponent.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
